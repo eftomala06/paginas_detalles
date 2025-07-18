d4c9b76176728bb46148032333c9bd5f7f246cbc
eftomala06 <erickftoch06@hotmail.com>
2025-07-18 16:34:36 -0500

    Add files via upload

---
+++ paginas/script.js
@@ -0,0 +1,8 @@
+function mostrarMensaje() {
+    const mensaje = `Mi amor, gracias por estar a mi lado. 
+  Eres mi alegría diaria, mi paz en los días difíciles y el amor de mi vida. 💖
+  
+  Te amo con todo mi corazón.`;
+    document.getElementById('mensaje').innerText = mensaje;
+  }
+  
